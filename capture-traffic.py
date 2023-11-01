9248b01fde8d075e9598c69f7435076044b288ad
Laurentiu Raducu <laurentiu.raducu@gmail.com>
2023-11-01 09:11:52 +0100

    Update capture-traffic.py

--- capture-traffic.py
+++ capture-traffic.py
@@ -3,6 +3,6 @@ from scapy.all import sniff
 def packet_callback(packet):
     print(packet.summary())
 
-packets = sniff(iface="eth0", count=100, prn=packet_callback)
+packets = sniff(iface="lo", count=100, prn=packet_callback)
 
 wrpcap("captured_packets.pcap", packets)
